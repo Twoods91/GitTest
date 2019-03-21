89b788934f00b0646c4658ff750ec3954d81c7ac
Twoods91 <twoods91@yahoo.com>
2019-03-21 14:16:20 -0400

    push
    asdfs
    
    -- INSERT --

---
+++ oddities.java
@@ -0,0 +1,17 @@
+import.java.util.*;
+public class oddities{
+	public static void main(String[] args){
+		int input = new Scanner(System.in);
+		int testcase = ars[0];
+		for(int i = 0; i < testcase; i++){
+			int numberToCheck = input.nextInt();
+			boolena isEven = false;
+			if(numberToCheck%2==0){
+				isEven = true;
+				System.out.println(numberToCheck + " is Even");
+			} else{
+				System.out.println(nuberToCheck + " is Odd");
+			}
+		}
+	}
+}
